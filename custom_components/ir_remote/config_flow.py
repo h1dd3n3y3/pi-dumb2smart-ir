2dc6e05dcacbf04902a82353f00ea713a7b94250
David Parathyras <parathyrasdavid@gmail.com>
2026-04-24 14:55:06 +0300

    allow integration setup with empty device list

--- custom_components/ir_remote/config_flow.py
+++ custom_components/ir_remote/config_flow.py
@@ -43,15 +43,12 @@ class IRRemoteConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     unsub()
 
                 if not errors:
-                    if not payload_holder.get("devices"):
-                        errors["base"] = "no_devices"
-                    else:
-                        await self.async_set_unique_id(prefix)
-                        self._abort_if_unique_id_configured()
-                        return self.async_create_entry(
-                            title=f"IR Remote ({prefix})",
-                            data=user_input,
-                        )
+                    await self.async_set_unique_id(prefix)
+                    self._abort_if_unique_id_configured()
+                    return self.async_create_entry(
+                        title=f"IR Remote ({prefix})",
+                        data=user_input,
+                    )
 
         return self.async_show_form(
             step_id="user",
